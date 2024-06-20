f00da4007433746b5d9bee88fae75fde7fa3ab4f
nik12224 <0lezgin5@gmail.com>
2024-06-20 17:28:35 +0500

    Добавляет средние и мелкие компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './scss/app.scss'
 
 function App() {
 	return <div className="App"></div>
---
+++ src/components/Vacansy/Vacansy.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Vacansy = () => {
+	return <div>Vacansy</div>
+}
+
+export default Vacansy
