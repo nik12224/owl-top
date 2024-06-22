6f27c7d3e45e0dc6a68e1e8b459c42da0efac6f2
nik12224 <0lezgin5@gmail.com>
2024-06-22 12:13:50 +0500

    Добавляет блок курса и стилизует его

--- src/components/Course/Course.tsx
+++ src/components/Course/Course.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/rectangle.png'
+import avatar from '../../assets/avatar.svg'
 import '../../scss/components/_course.scss'
 
 const Course: React.FC = () => {
@@ -71,12 +72,19 @@ const Course: React.FC = () => {
 					</p>
 					<div className="body__info">
 						<div className="body__left">
-							<b>Документ об окончании </b>
-							<span>Диплом</span>
-							<b>Сложность</b>
-							<span>Начальная</span>
-							<b>Длительность</b>
-							<span>5 месяцев</span>
+							<div className="body__left--block">
+								<b>Документ об окончании </b>
+								<span>Диплом</span>
+							</div>
+							<div className="body__left--block">
+								<b>Сложность</b>
+								<span>Начальная</span>
+							</div>
+
+							<div className="body__left--block">
+								<b>Длительность</b>
+								<span>5 месяцев</span>
+							</div>
 						</div>
 						<div className="body__right">
 							<b>Преимущества</b>
@@ -88,12 +96,33 @@ const Course: React.FC = () => {
 						</div>
 					</div>
 					<div className="body__links">
-						<a href="/">Узнать подробнее</a>
-						<a href="/">Читать отзывы</a>
+						<a className="link" href="/">
+							Узнать подробнее
+						</a>
+						<a className="link link__white" href="/">
+							Читать отзывы
+							<svg
+								width="6"
+								height="10"
+								viewBox="0 0 6 10"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg">
+								<path
+									d="M5.7154 4.6098L1.26527 0.159755C1.16235 0.0567475 1.02495 0 0.878448 0C0.731945 0 0.594548 0.0567475 0.491622 0.159755L0.163901 0.487394C-0.0493491 0.700889 -0.0493491 1.04788 0.163901 1.26105L3.90078 4.99793L0.159755 8.73895C0.0568288 8.84196 0 8.97928 0 9.1257C0 9.27228 0.0568288 9.4096 0.159755 9.51269L0.487476 9.84025C0.590483 9.94325 0.727799 10 0.874302 10C1.0208 10 1.1582 9.94325 1.26113 9.84025L5.7154 5.38614C5.81857 5.2828 5.87524 5.14484 5.87491 4.99817C5.87524 4.85094 5.81857 4.71305 5.7154 4.6098Z"
+									fill="#3B434E"
+								/>
+							</svg>
+						</a>
 					</div>
 				</div>
 			</div>
-			<div className="comments">Комментарии</div>
+			<div className="comments">
+				<div className="comments__header">
+					<div>
+						<img src={avatar} alt="" />
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
